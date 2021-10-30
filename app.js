75ac27aff006edd477d338fd1b3f32279157d957
webert maciel <webert_maciel@hotmail.com>
2021-10-29 21:05:52 -0300

    process 1

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const sharp = require('sharp')
+const compress_images = require('compress-images')
+
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 console.log(path, width);
@@ -8,7 +10,36 @@ function resize(path, width) {
             console.log(err)
         } else {
             console.log("Imagem redimencionada com sucesso!")
+            compress(outputPath, "./compressed");
         }
     });
 }
-resize(path, width)
+
+function compress(pathInput, outputPath) {
+
+    compress_images(pathInput, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+        function (error, completed, statistic) {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+        }
+    );
+
+
+}
+
+
+
+resize(path, "./temp/output_resize.jpg", width);
+
+
+
+
+
+
