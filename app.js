81fed1a0a362ff798f26f20b7e79fc071ebe4bbe
webert maciel <webert_maciel@hotmail.com>
2021-11-02 12:33:00 -0300

    first correction

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const compress_images = require('compress-images')
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 console.log(path, width);
-function resize(path, width) {
+function resize(path, outputPath, width) {
     sharp(path).resize({ width: width }).toFile('./temp/output_resize.jpg', (err) => {
         if (err) {
             console.log(err)
